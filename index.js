ccf8c44fc83690070b7029cdd6f37ab21ef6a913
David Chick <dchick@mac.com>
2024-11-29 18:29:36 -0800

    a bunch of stuff for the form

--- index.js
+++ index.js
@@ -1,44 +1,15 @@
-let testVar = 9;
+const shuffleButton = document.getElementById('shuffle');
+const selectBooze = document.getElementById('poison');
+const updatePlayer = document.getElementById('update-player');
 
-const doSomeMath = function(num) {
-    num += num - Math.pow(num, 3);
-    return num;
-};
-
-const eatTheDirt = function(line = 'nah') {
-
-    if (line === 'eat dirt') {
-      return 'yum';
-    } else {
-      return 'no thanks';
-    }
-  
-  };
-
-//console.log(doSomeMath(testVar));
-
-//let's build a form
-
-
-
-
-let myDeck = getNewDeck();
-let myCard = '';
-let counter = 1;
-
-while (myDeck.length) {
-  myDeck = drawACard(myDeck);
-  myCard = myDeck.shift();
-  //console.log(`counter: ${counter} my card: ${myCard.id} my deck has ${myDeck.length} cards left.`);
-  counter++;
-}
-
+shuffleButton.classList.add('hidden');
 
 const fetchDrinks = function(booze) {
 
   const drinksDiv = document.getElementById('drinks');
   const BASE_URL = 'https://www.thecocktaildb.com/api/json/v1/1/filter.php';
   let url = `${BASE_URL}?i=${booze}`;
+  booze === 'Non_Alcoholic'? url = `${BASE_URL}?a=${booze}`: `${BASE_URL}?i=${booze}`;
 
   fetch(url)
   .then(function(data) {
@@ -46,16 +17,85 @@ const fetchDrinks = function(booze) {
   })
   .then(function(responseJson) {
     console.log(responseJson);
-
-  let drinks = responseJson.drinks;
-  let drinksHTML = '';
-
-  for (let i=0; i < 3; i++) {
-    let rnd = Math.floor(Math.random() * drinks.length);
-    drinksHTML = drinksHTML.concat(`<div class="column">${drinks[rnd].strDrink}<br><img src="${drinks[rnd].strDrinkThumb}/preview"><br></div>`)
-  };
-    drinksDiv.innerHTML = drinksHTML;
+    let drinks = responseJson.drinks;
+    const shuffle = document.getElementById('shuffle');
+    shuffle.classList.remove('hidden');
+    shuffle.value = `Shuffle ${booze} Cocktails`;
+    while (drinksDiv.firstChild) {
+      drinksDiv.removeChild(drinksDiv.firstChild);
+    }
+    let picked = [];
+    for (let i=0; i < 5; i++) {
+      let rnd = Math.floor(Math.random() * drinks.length);
+      while (picked[rnd]) {
+        rnd = Math.floor(Math.random() * drinks.length);
+      }
+      picked[rnd] = true;
+      let cocktailName = drinks[rnd].strDrink;
+      let cocktailDiv = document.createElement('div');
+      cocktailDiv.classList.add('form-group');
+      cocktailDiv.classList.add('column');
+      cocktailDiv.setAttribute('id', cocktailName);
+      drinksDiv.append(cocktailDiv);
+      let p = document.createElement('p');
+      let cocktailText = document.createTextNode(cocktailName);
+      p.innerText = cocktailName;
+      cocktailDiv.appendChild(p);
+      let cocktailImg = document.createElement('img');
+      cocktailImg.setAttribute('src', `${drinks[rnd].strDrinkThumb}/preview`);
+      cocktailDiv.appendChild(cocktailImg);
+      cocktailDiv.addEventListener('click', (e) => {
+        for (const node of drinksDiv.childNodes) {
+          node.classList.remove('selected');
+        }
+        cocktailDiv.classList.add('selected');
+      });
+    };
   });
 };
 
-fetchDrinks('Gin');
+
+selectBooze.addEventListener('change', (e) => {
+
+  fetchDrinks(selectBooze.value);  
+
+});
+
+shuffleButton.addEventListener('click', (e) => {
+
+  fetchDrinks(selectBooze.value);
+
+});
+
+updatePlayer.addEventListener('click', (e) => {
+
+  e.preventDefault();
+
+  //validate form
+
+  let playerName = document.getElementById('name').value;
+  let drinkOfChoice = '';
+  let message = '';
+  const drinksDiv = document.getElementById('drinks');
+
+  for (const node of drinksDiv.childNodes) {
+      if (node.classList.contains('selected')) {
+        drinkOfChoice = node.childNodes[0].innerText;
+      }
+    }
+
+  message = `${playerName} is served a delicious ${drinkOfChoice}. Let's play cards!`;
+  
+  const messageDiv = document.getElementById('message');
+  const letsPlay = document.createTextNode(message);
+  const h3El = document.createElement('h3');
+  h3El.appendChild(letsPlay);
+  messageDiv.innerHTML = '';
+  messageDiv.appendChild(h3El);
+  console.log(message);
+
+});
+
+let myDeck = getNewDeck();
+let myCard = '';
+
